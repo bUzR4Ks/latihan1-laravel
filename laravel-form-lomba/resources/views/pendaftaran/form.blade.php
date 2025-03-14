4cc791aef47d4551b0f32de4e44cd2d0614cf316
bUzR4Ks <aulia.bilibili@gmail.com>
2025-03-14 10:13:07 +0700

    Update form.blade.php

--- laravel-form-lomba/resources/views/pendaftaran/form.blade.php
+++ laravel-form-lomba/resources/views/pendaftaran/form.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulir Pendaftaran</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEJ6XxS6CkF7X2b+BdGrQiw7tfd3V1uBq4Jh6w1M0PzPjhtfyXJoEwl7IcXLz" crossorigin="anonymous">
 </head>
 <body>
     <h1>Formulir Pendaftaran</h1>
@@ -30,5 +31,6 @@
             <button type="submit">Submit</button>
         </div>
     </form>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0gQ2B7+uJ3g3ckhBh6/Os7FfBOS3aZk8Gx5ZrZIZyyuV4ZSw" crossorigin="anonymous"></script>
+    </body>
 </html>
